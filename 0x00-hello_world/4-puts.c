8efd6cdfa5af912846c83bae4514d4bd8d26d97e
asresgamualx22 <asresgamualx22@example.com>
2022-12-12 00:25:03 -0800

    The first c rogramming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+/**
+ *This is my frist c programming to call function
+* */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+					return (0);
+
+}
+
+
