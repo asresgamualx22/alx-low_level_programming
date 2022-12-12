f64634f1487817b823f3c4d63169af50eccb9e81
asresgamualx22 <asresgamualx22@example.com>
2022-12-12 09:31:58 -0800

    this is my puts function use

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-/**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
- */
-int main(void)
-    {	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-    }
+#include <stdio.h>                                                                       
+/* Prints out a the main function usinf puts function.
+Return zero (0) of the function complet sucssesfrlly.
+*/
+int main(void);
+       {
+	       puts("\"Programming is like building a multilingual puzzle");
+	      
+	       return (0);
+       }	 
