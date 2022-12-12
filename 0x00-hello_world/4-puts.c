ae815d1e9ed6d379a6163a84af30686077e2fa76
asresgamualx22 <asresgamualx22@example.com>
2022-12-12 09:50:13 -0800

    punts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                                       
-/* Prints out a the main function usinf puts function.
-Return zero (0) of the function complet sucssesfrlly.
-*/
-int main(void);
-       {
-	       puts("\"Programming is like building a multilingual puzzle");
-	      
-	       return (0);
-       }	 
+#include <stdio.h>
+/* Print the main function using puts function
+ * Return zero after sucsessfull
+ */
+int main();
+{
+	punts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}	
+
