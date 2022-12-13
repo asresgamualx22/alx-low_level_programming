9aca7b959edd69c1267ccddfa1d04ef08c45b9f4
asresgamualx22 <asresgamualx22@example.com>
2022-12-13 05:48:38 -0800

    The putchar use in c programing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* This is my frist use in _putchar */
+int main(viod)
+{ 
+	// the decelaration of char function to revoke
+	char alx = "ALX is the best shool in Afirca";
+	putchar(alx); // display character alx
+	return(0);
+
+}
