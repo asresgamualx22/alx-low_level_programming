aa0f74253429f3de0079d386af61b7c6fb727806
asresgamualx22 <asresgamualx22@example.com>
2022-12-18 01:52:07 -0800

    punts last update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- * main point 
- *
+ * main point
  * Return: Alwasys 0
- */
+*/
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
