f724db99ccaed97730ddc8b5beeceeb8cff9dbd6
asresgamualx22 <asresgamualx22@example.com>
2022-12-18 03:04:43 -0800

    Punts last update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * main point
- * Return: Alwasys return 0 if the task is complleted scusses 
+ * Description: main - Prints out a string to stdout.
+ *
+ * Return: 0 if success.
 */
+
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
